4a532aa51c7ed48a074693e1b84f444f3cdf31d3
Brayan Cantos <brayancantosm07ci@gmail.com>
2025-03-23 18:37:22 -0500

    📝 (index.js): add setupSwagger function to initialize Swagger documentation for the app 🐛 (index.js): fix console log message to include the actual port number where the API is running

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const app = require('./src/app');
 const PORT = process.env.PORT || 3000;
+const setupSwagger = require('./swagger');
+
+setupSwagger(app);
 
 app.listen(PORT, () => {
-    console.log(`API running at http://localhost:${PORT}`);
+    console.log(`API running at ${PORT}`);
 });
