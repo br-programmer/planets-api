833707ac8efb1d05e4362a4aed7ec148e9d83d84
Brayan Cantos <brayancantosm07ci@gmail.com>
2025-03-23 18:13:05 -0500

    ✨ (planets.routes.js): add apiKeyAuth middleware to authenticate requests using an API key before accessing planet routes

--- src/routes/planets.routes.js
+++ src/routes/planets.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { getAllPlanets, getPlanetById } = require('../controllers/planets.controller');
+const apiKeyAuth = require('../middleware/api.key.auth');
+
+router.use(apiKeyAuth);
 
 router.get('/', getAllPlanets);
 router.get('/:id', getPlanetById);
