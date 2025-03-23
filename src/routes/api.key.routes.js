9d04f74e00ebbf1f50a8b3eb0b4f9ea8caabd6e9
Brayan Cantos <brayancantosm07ci@gmail.com>
2025-03-23 18:13:19 -0500

    ✨ (api.key.routes.js): add authentication middleware to protect routes and improve security 📝 (api.key.routes.js): refactor route to use more descriptive endpoint for generating API key

--- src/routes/api.key.routes.js
+++ src/routes/api.key.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { generateApiKey, listApiKeys } = require('../controllers/api.key.controller');
+const authToken = require('../middleware/auth.token');
 
-router.post('/', generateApiKey);
-
+router.use(authToken);
+router.post('/generate', generateApiKey);
 router.get('/list', listApiKeys);
 
 module.exports = router;
